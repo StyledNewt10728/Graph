74f6f236e5e91aee0c8400beeae2eaa4495bd9ca
StyledNewt10728 <118925158+StyledNewt10728@users.noreply.github.com>
2025-06-20 12:09:56 -0400

    Finished graph.js
    Finished Graph project completed after mentor meeting

--- graph.js
+++ graph.js
@@ -1,5 +1,3 @@
-// I know that this is just the blank started code,  this is because I really have no idea how to start. I have looked at the solution but it seems very vauge and not right and the graph exercises 'paper' with waht needs to happen also doesn't really show anything.  I've tried looking through the videos but I am just not able to understand what is needed to be done.
-
 class Node {
   constructor(value, adjacent = new Set()) {
     this.value = value;
@@ -13,25 +11,118 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+      }
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const stack = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    visited.add(start);
+
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach((neighbor) => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    visited.add(start);
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach((neighbor) => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  shortestPath(start, end) {
+    if (start === end) {
+      return [start.value];
+    }
+
+    var queue = [start];
+    let visited = new Set();
+    let predecessors = {};
+    let path = [];
+
+    while (queue.length) {
+      let currentVertex = queue.shift();
+
+      if (currentVertex === end) {
+        let stop = predecessors[end.value];
+        while (stop) {
+          path.push(stop);
+          stop = predecessors[stop];
+        }
+        path.unshift(start.value);
+        path.reverse();
+        return path;
+      }
+
+      visited.add(currentVertex);
+      for (let vertex of currentVertex.adjacent) {
+        if (!visited.has(vertex)) {
+          predecessors[vertex.value] = currentVertex.value;
+          queue.push(vertex);
+        }
+      }
+    }
+  }
 }
 
 module.exports = { Graph, Node };
